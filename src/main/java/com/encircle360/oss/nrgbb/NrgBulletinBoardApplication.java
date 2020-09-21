a3f87171e0a0f3541a6e42bb863fcc73fe39f1dc
peter <ps@web-eifel.de>
2020-09-21 10:20:48 +0200

    project setup

---
+++ src/main/java/com/encircle360/oss/nrgbb/NrgBulletinBoardApplication.java
@@ -0,0 +1,12 @@
+package com.encircle360.oss.nrgbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrgBulletinBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NrgBulletinBoardApplication.class).start();
+    }
+}
