d48f4dbc6c7761ea0cd58a00f6efdf290ba85ceb
peter <ps@web-eifel.de>
2020-09-22 11:47:31 +0200

    added insecure profile

---
+++ src/main/java/com/encircle360/oss/nrgbb/security/NoSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.encircle360.oss.nrgbb.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+
+@Configuration
+@EnableWebSecurity
+@Profile("insecure")
+public class NoSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security
+            .csrf().disable()
+            .cors().and()
+            .sessionManagement()
+            .sessionAuthenticationStrategy(new NullAuthenticatedSessionStrategy())
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers()
+            .permitAll()
+            .anyRequest()
+            .anonymous();
+    }
+}
--- src/main/java/com/encircle360/oss/nrgbb/security/OauthSecurityConfig.java
+++ src/main/java/com/encircle360/oss/nrgbb/security/OauthSecurityConfig.java
@@ -2,6 +2,7 @@ package com.encircle360.oss.nrgbb.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.session.NullAuthenticated
 
 @Configuration
 @EnableWebSecurity
+@Profile("!insecure")
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class OauthSecurityConfig extends WebSecurityConfigurerAdapter {
 
