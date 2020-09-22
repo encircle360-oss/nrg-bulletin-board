9958932c3200b351d7182d71134d48040b26cf18
peter <ps@web-eifel.de>
2020-09-22 10:20:54 +0200

    added first version with jwt

---
+++ src/main/java/com/encircle360/oss/nrgbb/config/security/OauthSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.encircle360.oss.nrgbb.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class OauthSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security
+            .csrf().disable()
+            .cors().and()
+            .sessionManagement()
+            .sessionAuthenticationStrategy(new NullAuthenticatedSessionStrategy())
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .oauth2ResourceServer(configurer -> configurer.jwt().jwtAuthenticationConverter(extractorConverter()));
+    }
+
+    @Bean
+    Converter<Jwt, AbstractAuthenticationToken> extractorConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesExtractor());
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    GrantedAuthoritiesExtractor grantedAuthoritiesExtractor() {
+        return new GrantedAuthoritiesExtractor();
+    }
+}
